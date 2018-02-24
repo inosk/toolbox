d1005958a201eb4ce6d4dd00af884805035ab1c1
inosk <inoue.yusuke.928@gmail.com>
2018-02-24 17:58:45 +0900

    ワンラインでできそうだったので修正

--- zsh/git_filter_branch.zsh
+++ zsh/git_filter_branch.zsh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 # gitのコミッター情報を全部書き換えるやつ
 
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='inosk'; GIT_COMMITER_NAME='inosk'"
-git filter-branch -f --env-filter "GIT_AUTHOR_EMAIL='inoue.yusuke.928@gmail.com'"
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='inosk'; GIT_COMMITTER_NAME='inosk'; GIT_AUTHOR_EMAIL='inoue.yusuke.928@gmail.com'; GIT_COMMITTER_EMAIL='inoue.yusuke.928@gmail.com'"
